cd7d30375167d89afcc25602e562db0b595ba7b1
Kang-min Liu <gugod@gugod.org>
2019-02-26 09:19:37 +0900

    use some computed geometry.

--- eg/test-binarize.pl
+++ eg/test-binarize.pl
@@ -13,6 +13,15 @@ $img = $img->convert( preset => "noalpha" )->convert( matrix => [[0.333, 0.333, 
 $img->write( file => "o-grey.png" );
 say "Written o-grey.png";
 
+my ($w, $h) = ( $img->getwidth(), $img->getheight() );
+$w = int( $w / 100 );
+$h = int( $h / 100 );
+$w = 5 if $w < 5;
+$h = 5 if $h < 5;
+
+my $geo = $w . 'x' . $h;
+say "Geometry: $geo";
+
 my @kids;
 for my $method ('sauvola', 'niblack') {
     if (my $pid = fork()) {
@@ -21,7 +30,7 @@ for my $method ('sauvola', 'niblack') {
         $img->filter(
             type => "binarization",
             method => $method,
-            geometry => "5x5",
+            geometry => $geo,
         ) or die $img->errstr;
         $img->write( file => "o-${method}.png" );
         say "Written o-${method}.png";
